65d91b2d00fb7ca0922d88c87e72de5176136289
Quinn <quinnthepigeon@proton.me>
2025-06-19 18:21:57 +0200

    add error handling for if we cannot get the random data

--- src/main.c
+++ src/main.c
@@ -57,7 +57,8 @@ int main(int argc, char** argv) {
 		}
 
 		// populate the array with random data
-		getrandom(rdat->dat, c + !!mod, 0);
+		if (getrandom(rdat->dat, c + !!mod, 0) < 0)
+			error(1, "failed to acquire random data!\n", );
 
 		// perform for the input count
 		ull headsc = 0; // amount of heads
